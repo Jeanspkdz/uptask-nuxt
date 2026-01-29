04e17d80f7e2cdc75961c5338e9396aaf292dcc5
jeanspkdz <jeanchua1304@gmail.com>
2026-01-29 12:29:15 -0500

    fix: prevent layout shift by setting logo aspect-ratio

--- app/layouts/home-layout.vue
+++ app/layouts/home-layout.vue
@@ -2,7 +2,7 @@
   <div class="min-h-dvh flex flex-col">
     <header class="w-full max-w-dvw bg-slate-900">
       <div class="flex justify-between items-center mx-auto max-w-10/12 py-3">
-        <NuxtImg src="/logo.svg" width="200" height="40" class="-ms-4" />
+        <NuxtImg src="/logo.svg" width="200" height="40" class="-ms-4 aspect-[285/100]" />
         <button class="hover:cursor-pointer bg-violet-400 p-1">
           <Menu class="text-white"  width="16" height="16"/>
         </button>
